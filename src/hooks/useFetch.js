65f2a2907f0a44fc05ec3e416cfece4663e1f262
baryael <barbar6132@gmail.com>
2023-09-21 19:11:24 +0300

    'Movie_API'

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+
+
+export const useFetch = (apiPath, queryTerm="") => {
+  const API = "b80d59c33d6d57ed9c7e3713f91c188a"
+
+  
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${API}&query=${queryTerm}`
+
+
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
